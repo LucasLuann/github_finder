f880b9fba42e6af117fd5bd2a6af0e538bff4774
Lucas Luann <lucasluann10@gmail.com>
2024-09-03 04:34:35 -0300

    Resgatando o usuário pela api do github

--- github_finder/src/components/Busca.tsx
+++ github_finder/src/components/Busca.tsx
@@ -1,17 +1,28 @@
-import React from 'react'
+type SearchProps = {
+  carregarUsuario: (userName: string) => Promise<void>;
+};
+
+import React from "react";
+import { useState } from "react";
 // import { BsSearch } from "react-icons/bs"
 
-const Busca = () => {
+const Busca = ({ carregarUsuario }: SearchProps) => {
+  const [userName, setUserName] = useState("");
+
   return (
     <div>
-        <h2>Buscar Usuário</h2>
-        <p>Repositórios mais avaliados</p>
-        <div>
-            <input type="text" placeholder='Digite o nome do usuário' />
-            <button>Buscar</button>
-        </div>
+      <h2>Buscar Usuário</h2>
+      <p>Repositórios mais avaliados</p>
+      <div>
+        <input
+          type="text"
+          placeholder="Digite o nome do usuário"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <button onClick={() => carregarUsuario(userName)}>Buscar</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Busca
+export default Busca;
--- github_finder/src/routes/Home.tsx
+++ github_finder/src/routes/Home.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import { UserProps } from "../types/user";
+import { useState } from "react";
 import Busca from "../components/Busca";
 
 const Home = () => {
+  const [user, setUser] = useState<UserProps | null>(null);
+
+  const carregarUsuario = async (userName: string) => {
+    const res = await fetch(`https://api.github.com/users/${userName}`)
+    const data = await res.json()
+
+    console.log(data)
+  };
+
   return (
     <div>
-      <Busca />
+      <Busca carregarUsuario={carregarUsuario} />
     </div>
   );
 };
